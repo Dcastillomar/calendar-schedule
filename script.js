83049ef626efa5ce92f4dc13be2755ba5e22b7cd
Dcastillomar <125750537+Dcastillomar@users.noreply.github.com>
2023-04-20 21:49:18 -0500

    save button pulls from local storage and shows on main page

--- script.js
+++ script.js
@@ -13,14 +13,20 @@ $(function () {
 
   console.log(realTime);
 
+  //pulls from local storage
+$("#hour-8 .description").val(localStorage.getItem("hour-8"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+$("#hour-18 .description").val(localStorage.getItem("hour-18"));
 
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   
 
   //SAVE BUTTON
@@ -29,7 +35,6 @@ $(function () {
     var time = $(this).parent().attr("id");
     
     localStorage.setItem(time, textValue);
-    localStorage.getItem();
   })
 
 
@@ -133,17 +138,5 @@ $(function () {
   } else {
     document.getElementById("hour-18").style.backgroundColor = '#d3d3d3';
   }
-
-
-  //loop?
-
-
-
-  
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  
-
 
 });
